8bcc119d902babd26579a5c622ae6cf3544bf705
BrandonQ2 <branqq1598@gmail.com>
2019-08-07 18:38:22 -0500

    Pantalla Docentes

--- app/src/main/java/com/example/apppermisos/DocentesActivity.java
+++ app/src/main/java/com/example/apppermisos/DocentesActivity.java
@@ -85,17 +85,9 @@ public class DocentesActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_home) {
+        if (id == R.id.nav_Solicitar) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_tools) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_mispermisos) {
 
         }
 
