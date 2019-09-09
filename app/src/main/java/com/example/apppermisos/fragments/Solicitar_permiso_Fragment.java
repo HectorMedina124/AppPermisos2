c7f833d74aa3836e5c74437cba825ffcbf915e44
Hector Aurelio Medina Cornejo <hector.medina.est@gmail.com>
2019-09-08 23:07:02 -0500

    Ya registra permisos

--- app/src/main/java/com/example/apppermisos/fragments/Solicitar_permiso_Fragment.java
+++ app/src/main/java/com/example/apppermisos/fragments/Solicitar_permiso_Fragment.java
@@ -6,13 +6,13 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
-import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
 import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
@@ -25,26 +25,25 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.example.apppermisos.DatePickerFragment;
 import com.example.apppermisos.R;
-import com.example.apppermisos.objetos.Permiso;
 import com.example.apppermisos.objetos.Persona;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
 
-public class Solicitar_permiso_Fragment extends Fragment implements DatePickerDialog.OnDateSetListener {
+public class Solicitar_permiso_Fragment extends Fragment {
     private Persona per;
     private Persona per2;
-    private boolean correcto=false;
+    private boolean correcto;
     private TextView  solicitante;
     private TextView fechaSolicitud;
     private Spinner tipoPermiso;
@@ -59,8 +58,10 @@ public class Solicitar_permiso_Fragment extends Fragment implements DatePickerDi
     private Button enviar;
     private Button cancelar;
     private RequestQueue requestQueue;
+    private ArrayList<Persona> aprobadores;
     private String rfc2;
     private PermisosFrag.OnFragmentInteractionListener mListener;
+    private ArrayList<String> nombres;
 
     public Solicitar_permiso_Fragment() {
         // Required empty public constructor
@@ -89,21 +90,82 @@ public class Solicitar_permiso_Fragment extends Fragment implements DatePickerDi
         cancelar=vista.findViewById(R.id.btn_cancelar);
         fechaini=vista.findViewById(R.id.spinner_date);
         fechafin=vista.findViewById(R.id.spinner_datef);
-        SimpleDateFormat simpleDateFormat= new SimpleDateFormat("dd-MM-yyyy");
+        solicitante.setText(per.getNombre()+" "+per.getApellidoPaterno()+" "+per.getApellidoMaterno());
+        SimpleDateFormat simpleDateFormat= new SimpleDateFormat("yyyy-MM-dd");
+        Date date= Calendar.getInstance().getTime();
+        String fechaSol = simpleDateFormat.format(date);
+        fechaSolicitud.setText(fechaSol);
+        traerAprobadores("http://puntosingular.mx/app_permisos/consultardirectivos.php");
+        Handler han= new Handler();
+        han.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+              nombres= new ArrayList<String>();
+                for(int i=0;i<aprobadores.size();i++){
+                    nombres.add(aprobadores.get(i).getNombre()+" "+aprobadores.get(i).getApellidoPaterno()+" "+aprobadores.get(i).getApellidoMaterno());
+                }
+                ArrayAdapter<String> items= new ArrayAdapter<String>(getActivity(),android.R.layout.simple_spinner_item,nombres);
+                destinatario.setAdapter(items);
+            }
+        },2000);
+
 
         fechaini.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                DialogFragment dialogFragment= new DatePickerFragment();
-             dialogFragment.show(getActivity().getSupportFragmentManager(),"Elegir fecha");
-
+                Calendar c= Calendar.getInstance();
+                int diai,mesi,anioi;
+                anioi=c.get(Calendar.YEAR);
+                mesi=c.get(Calendar.MONTH);
+                diai=c.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog datePickerDialog=new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int anio, int mes, int dia) {
+                        String fechaelegida="";
+                        fechaelegida+=""+anio;
+                        if(mes<10) {
+                            fechaelegida+="-0"+(mes+1);
+                        }else{
+                            fechaelegida+="-"+(mes+1);
+                        }
+                        if(dia<10){
+                            fechaelegida+="-0"+dia;
+                        }else{
+                            fechaelegida+="-"+dia;
+                        }
+                        fechaini.setText(fechaelegida);
+                    }
+                },anioi,mesi,diai);
+                datePickerDialog.show();
             }
         });
         fechafin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                DialogFragment dialogFragment= new DatePickerFragment();
-                dialogFragment.show(getActivity().getSupportFragmentManager(),"Elegir fecha");
+                Calendar c= Calendar.getInstance();
+                int diaf,mesf,aniof;
+                aniof=c.get(Calendar.YEAR);
+                mesf=c.get(Calendar.MONTH);
+                diaf=c.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog datePickerDialog=new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int anio, int mes, int dia) {
+                        String fechaelegida="";
+                        fechaelegida+=""+anio;
+                        if(mes<10) {
+                            fechaelegida+="-0"+(mes+1);
+                        }else{
+                            fechaelegida+="-"+(mes+1);
+                        }
+                        if(dia<10){
+                            fechaelegida+="-0"+dia;
+                        }else{
+                            fechaelegida+="-"+dia;
+                        }
+                        fechafin.setText(fechaelegida);
+                    }
+                },aniof,mesf,diaf);
+                datePickerDialog.show();
             }
         });
         cancelar.setOnClickListener(new View.OnClickListener() {
@@ -112,43 +174,53 @@ public class Solicitar_permiso_Fragment extends Fragment implements DatePickerDi
                 getActivity().onBackPressed();
             }
         });
-        solicitante.setText(per.getNombre()+" "+per.getApellidoPaterno()+" "+per.getApellidoMaterno());
-        Date date= Calendar.getInstance().getTime();
-        String fechas = simpleDateFormat.format(date);
-        fechaSolicitud.setText(fechas);
-        //rfc2=traerRfcAprobador();
-        //hacerSolicitud("http://puntosingular.mx/app_permisos/hacersolicitud?rfcsolicitante="+per.getRfc().toString()+"&fechaini="+fechainicio+"&fechafin="+fechafinal+"&horaI="+horainicio+"&horaF="+horafinal+"&fechasolicitud=curdate()&fechaAprobacion=null&cveAutoriza="+rfc2+"&tipopermiso="+(tipoPermiso.getSelectedItemId()+1)+"&descripcion="+descripcion.getText().toString()+"");
+        enviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int aprobador=Integer.parseInt(destinatario.getSelectedItemId()+"");
+                rfc2=aprobadores.get(aprobador).getClave();
+                hacerSolicitud("http://puntosingular.mx/app_permisos/hacersolicitud?rfcsolicitante="+per.getRfc()+"&fechaini="+fechaini.getText().toString()+"&fechafin="+fechafin.getText().toString()+"&horaI="+horaInicio.getSelectedItem().toString().substring(0,2)+":"+minutoInicio.getSelectedItem().toString()+":00"+"&horaF="+horaFin.getSelectedItem().toString().substring(0,2)+":"+minutoFin.getSelectedItem().toString()+":00"+"&fechasolicitud=curdate()&fechaAprobacion=null&cveAutoriza="+rfc2+"&tipopermiso="+(tipoPermiso.getSelectedItemId()+1)+"&descripcion="+descripcion.getText().toString()+"");
+            }
+        });
         return vista;
     }
-    public String traerRfcAprobador(String url){
+    public void traerAprobadores(String url){
         JsonArrayRequest jsonArrayRequest2= new JsonArrayRequest(url, new Response.Listener<JSONArray>() {
             @Override
             public void onResponse(JSONArray response) {
                 JSONObject jsonObject = null;
-                per2=new Persona();
+                aprobadores= new ArrayList<Persona>();
                 for (int i = 0; i < response.length(); i++) {
                     try {
                         jsonObject = response.getJSONObject(i);
+                        per2=new Persona();
+                        per2.setClave(jsonObject.getString("cve_per"));
+                        per2.setNombre(jsonObject.getString("nom_per"));
+                        per2.setApellidoPaterno(jsonObject.getString("ap_per"));
+                        per2.setApellidoMaterno(jsonObject.getString("am_per"));
                         per2.setRfc(jsonObject.getString("rfc_per"));
+                        aprobadores.add(per2);
                     } catch (JSONException e) {
+                        Toast.makeText(getActivity(), e.toString(), Toast.LENGTH_SHORT).show();
                         e.printStackTrace();
                     }
                 }
+
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                // Toast.makeText(Login_Activity.this, error.toString(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(), error.toString(), Toast.LENGTH_SHORT).show();
             }
         });
         requestQueue= Volley.newRequestQueue(getContext());
         requestQueue.add(jsonArrayRequest2);
-        return per2.getRfc().toString();
+
     }
     public void hacerSolicitud(String url){
-        JsonArrayRequest jsonArrayRequest2= new JsonArrayRequest(url, new Response.Listener<JSONArray>() {
+        StringRequest stringRequest= new StringRequest(url, new Response.Listener<String>() {
             @Override
-            public void onResponse(JSONArray response) {
+            public void onResponse(String response) {
                 Toast.makeText(getContext(),"Su solicitud ah sido registrada",Toast.LENGTH_LONG).show();
             }
         }, new Response.ErrorListener() {
@@ -159,17 +231,9 @@ public class Solicitar_permiso_Fragment extends Fragment implements DatePickerDi
             }
         });
         requestQueue= Volley.newRequestQueue(getContext());
-        requestQueue.add(jsonArrayRequest2);
+        requestQueue.add(stringRequest);
     }
 
-    @Override
-    public void onDateSet(DatePicker datePicker, int anio, int mes, int diadelmes) {
-        Calendar c= Calendar.getInstance();
-        c.set(Calendar.YEAR,anio);
-        c.set(Calendar.MONTH,mes);
-        c.set(Calendar.DAY_OF_MONTH,diadelmes);
-        String fechaactual= DateFormat.getDateInstance().format(c.getTime());
-    }
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
